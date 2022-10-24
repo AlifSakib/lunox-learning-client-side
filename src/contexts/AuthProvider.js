d1c740e191ab7c57b9b8cbdace2801a85398961f
AlifSakib <alifsakib@gmail.com>
2022-10-25 05:06:36 +0600

    Set current user if state change

--- src/contexts/AuthProvider.js
+++ src/contexts/AuthProvider.js
@@ -10,24 +10,33 @@ import app from "../firebase/firebase.config";
 export const AuthContext = createContext();
 const auth = getAuth(app);
 const AuthProvider = ({ children }) => {
+  //? States
   const [user, setUser] = useState(null);
+  const [loading, setLoading] = useState(true);
 
   //? Create New User
   const createUser = (email, password) => {
+    setLoading(true);
     return createUserWithEmailAndPassword(auth, email, password);
   };
 
+  //? Login User
   const logInUser = (email, password) => {
+    setLoading(true);
     return signInWithEmailAndPassword(auth, email, password);
   };
 
+  //? LogOut User
   const logOutUser = () => {
+    setLoading(true);
     return signOut(auth);
   };
 
+  //? User State
   useEffect(() => {
     const unsubscribe = onAuthStateChanged(auth, (createUser) => {
       setUser(createUser);
+      setLoading(false);
     });
     return () => unsubscribe();
   }, []);
